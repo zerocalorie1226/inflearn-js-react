83d5d3bcf5523b8b7a023284d4c2f0b6ac0b1020
Sungchan CHo <kjyy07@naver.com>
2023-02-20 15:37:55 +0900

    단락회로평가

---
+++ 단락회로평가/index.js
@@ -0,0 +1,8 @@
+const getName = (person) => {
+  const name = person && person.name;
+  return name || "객체가 아닙니다.";
+};
+
+let person = { name: "조성찬" };
+const name = getName(person);
+console.log(name);
