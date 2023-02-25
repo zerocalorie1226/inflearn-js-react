c7ab4ca60fe9ffe832490cb8c818323aab081f01
Sungchan CHo <kjyy07@naver.com>
2023-02-25 17:57:13 +0900

    Create React App

---
+++ ReactPart/reactexam1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  let name = "조성찬";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 {name} </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
