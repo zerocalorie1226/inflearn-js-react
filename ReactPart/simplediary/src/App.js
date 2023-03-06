141b756dba81c009239feee020caefea0fa2f016
Sungchan CHo <kjyy07@naver.com>
2023-03-06 20:04:19 +0900

    React 배열 사용하기 데이터 삭제

--- ReactPart/simplediary/src/App.js
+++ ReactPart/simplediary/src/App.js
@@ -3,44 +3,6 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-/* const dummyList = [
-  {
-    id: 1,
-    author: "조성찬",
-    content: "하이 1",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "김진우",
-    content: "하이 2",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "김도현",
-    content: "하이 3",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "현지수",
-    content: "하이 4",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 5,
-    author: "채하은",
-    content: "하이 5",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-];
- */
 const App = () => {
   const [data, setData] = useState([]);
 
@@ -59,10 +21,16 @@ const App = () => {
     setData([newItem, ...data]);
   };
 
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
     </div>
   );
 };
--- ReactPart/simplediary/src/DiaryItem.js
+++ ReactPart/simplediary/src/DiaryItem.js
@@ -1,4 +1,11 @@
-const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+const DiaryItem = ({
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+  onDelete,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -9,6 +16,16 @@ const DiaryItem = ({ author, content, created_date, emotion, id }) => {
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          console.log(id);
+          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
